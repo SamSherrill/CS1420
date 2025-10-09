5768d6476cb490c6893f67d83ee3ffb128475fdc
Sam Sherrill <samuel.sherrill@gmail.com>
2025-10-09 10:53:47 -0600

    Recursion exercises

--- in_class/recursion.py
+++ in_class/recursion.py
@@ -1,7 +1,22 @@
 # 10/9/25 class session
 
 def factorial(n):
-    if n == 0:
+    if n == 0 or n == 1:
         return 1
+    return n * factorial(n - 1)
+    
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+    
+def main():
+    input_number = int(input("Enter a non-negative integer: "))
+    if input_number < 0:
+        print("Factorial is not defined for negative numbers.")
     else:
-        return n * factorial(n - 1)
+        print(f"The factorial of {input_number} is {factorial(input_number)}.")
+    print(f"The {input_number}th Fibonacci number is {fibonacci(input_number)}.")
+
+if __name__ == "__main__":
+    main()
