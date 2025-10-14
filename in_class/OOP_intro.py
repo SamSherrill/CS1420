78fb467aad736aa7ad1e06d3a81021206b90d40a
Sam Sherrill <samuel.sherrill@gmail.com>
2025-10-14 10:51:00 -0600

    Class method exercises.

--- in_class/OOP_intro.py
+++ in_class/OOP_intro.py
@@ -3,13 +3,22 @@
 class Car:
     wheel = 4 # class variable / property / attribute (all correct terms)
     # shared by all instances of the class
-    def __init__(self, color, model):
+    def __init__(self, color, model, speed=0):
         self.color = color
         self.model = model
+        self.speed = speed
+    def __str__(self):
+        return f"The color of this car is {self.color} and the model is {self.model}"
+    def moving(self, val):
+        self.speed = val
 
 c = Car("red", "2020")
 d = Car("blue", "2021")
 
-print(c.color)
-print(d.color)
+# print(f"{c.color}, {c.model}")
+# print(f"{d.color}, {d.model}")
+print(c)
+print(d)
 
+c.moving(25)
+print(f"The speed of the car is {c.speed} mph")
