36096c78cb81bd6c7b0fa42bcfdfb6c9fb756b66
Sam Sherrill <samuel.sherrill@gmail.com>
2025-11-04 10:20:54 -0700

    Starting exercises on stack, push, and pop.

--- in_class/typing_.py
+++ in_class/typing_.py
@@ -1,20 +1,42 @@
-# Class 10/30/25
+# Class 10/30/25 about typing, then continuing 11/4/25 with stack, generics, and typevars
 
-from typing import Union, Optional
+from typing import Union, Optional, TypeVar, Generic
 Vector = list[float]
 
-def add(a:Union[int, float], b:Union[int, float]) -> Union[int, float]:
-    return a + b
+# def add(a:Union[int, float], b:Union[int, float]) -> Union[int, float]:
+#     return a + b
 
-def vector_sum(a:Vector) -> float:
-    return sum(a)
+# def vector_sum(a:Vector) -> float:
+#     return sum(a)
 
-# Optional below is used in place of Union[type, None]
-def print_students(student:list) -> Optional[str]:
-    if len(student) > 0:
-        for i in student:
-            print(i)
-    else:
-        return None
+# # Optional below is used in place of Union[type, None]
+# def print_students(student:list) -> Optional[str]:
+#     if len(student) > 0:
+#         for i in student:
+#             print(i)
+#     else:
+#         return None
     
-# Next class we'll discuss generic type
+T = TypeVar("T")
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self.items = []
+
+    def push(self, item:T) -> None:
+        self.items.append(item)
+
+    def pop(self) -> T:
+        return self.items.pop()
+    
+int_stack = Stack[int]()
+int_stack.push(13)
+int_stack.push(22)
+print(int_stack.pop())
+print(int_stack.pop())
+
+str_stack = Stack[str]()
+str_stack.push("hello")
+str_stack.push("world")
+print(str_stack.pop())
+print(str_stack.pop())
