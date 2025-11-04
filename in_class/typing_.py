4ed2453f5122d487cd3525972b68d62ee7606270
Sam Sherrill <samuel.sherrill@gmail.com>
2025-11-04 10:43:15 -0700

    Further exercises on typing.

--- in_class/typing_.py
+++ in_class/typing_.py
@@ -1,6 +1,7 @@
 # Class 10/30/25 about typing, then continuing 11/4/25 with stack, generics, and typevars
 
-from typing import Union, Optional, TypeVar, Generic
+from enum import Enum
+from typing import Union, Optional, TypeVar, Generic, Literal
 Vector = list[float]
 
 # def add(a:Union[int, float], b:Union[int, float]) -> Union[int, float]:
@@ -32,11 +33,37 @@ class Stack(Generic[T]):
 int_stack = Stack[int]()
 int_stack.push(13)
 int_stack.push(22)
-print(int_stack.pop())
-print(int_stack.pop())
+# print(int_stack.pop())
+# print(int_stack.pop())
 
 str_stack = Stack[str]()
 str_stack.push("hello")
 str_stack.push("world")
-print(str_stack.pop())
-print(str_stack.pop())
+# print(str_stack.pop())
+# print(str_stack.pop())
+
+Payable = Literal["CARD", "CASH", "PHONE"]
+
+class Payable(Enum):
+    CARD = 1
+    CASH = 2
+    PHONE = 3
+
+# print(Payable.CARD.name) # .name give CARD; only Payable.CARD gives Payable.CARD
+# print(Payable.CARD.value) # .value also gives 1
+for i in Payable:
+    print(i, i.value)
+
+print(type(Payable.CARD))
+
+class Status(Enum):
+    PENDING = 1
+    IN_PROGRESS = 2
+    DONE = 3
+
+class Task:
+    def __init__(self, name:str, status:Status=Status.PENDING) -> None:
+        self.name = name
+        self.status = status
+    def __str__(self) -> str:
+        return f"Task(name={self.name}, status={self.status.name})"
