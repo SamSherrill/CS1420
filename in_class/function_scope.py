78487e6d1e5464925058aa276aa5d35dd9653778
Sam Sherrill <samuel.sherrill@gmail.com>
2025-11-13 10:11:58 -0700

    Moved some files to old_exercises. Started simulation.py hw. Starting in class activities on function scope.

---
+++ in_class/function_scope.py
@@ -0,0 +1,17 @@
+# Class 11/13/25
+
+def area_triangle(base: float, height: float) -> float:
+    return 0.5 * base * height
+def area_multiple_triangle(n, base: float, height: float) -> float:
+    one_triangle = area_triangle(base, height)
+    return one_triangle * n
+
+def main():
+    base = float(input("Enter the base of the triangle: "))
+    height = float(input("Enter the height of the triangle: "))
+    n = int(input("Enter the number of triangles: "))
+    total_area = area_multiple_triangle(n, base, height)
+    print(f"The total area of {n} triangles is: {total_area}")
+
+if __name__ == "__main__":
+    main()
